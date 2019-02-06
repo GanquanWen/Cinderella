013959e4a65c315f66160bfc8e688dfa294c3c61
DDOwzw <zwwang0816@gmail.com>
2019-02-05 23:50:09 -0600

    change db name

--- src/main/java/com/cinderella/db/mysql/MySQLDBUtil.java
+++ src/main/java/com/cinderella/db/mysql/MySQLDBUtil.java
@@ -3,7 +3,7 @@ package main.java.com.cinderella.db.mysql;
 public class MySQLDBUtil {
 	private static final String HOSTNAME = "localhost";
 	private static final String PORT_NUM = "3306"; // mysql port #
-	public static final String DB_NAME = "laundry";
+	public static final String DB_NAME = "cinderella";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
 	public static final String URL = "jdbc:mysql://" + HOSTNAME 
