00c04f53aca8da6f3208f62aa4ddf4bdeaf04e61
DDOwzw <zwwang0816@gmail.com>
2019-02-05 23:20:20 -0600

    add DB package

---
+++ src/main/java/com/cinderella/db/mysql/MySQLConnection.java
@@ -0,0 +1,180 @@
+package main.java.com.cinderella.db.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import main.java.com.cinderella.db.DBConnection;
+import main.java.com.cinderella.db.mysql.MySQLDBUtil;
+
+
+public class MySQLConnection implements DBConnection {
+	private Connection conn;
+
+	public MySQLConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void close() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void setFavoriteItems(String userId, List<String> itemIds) {
+		if (conn == null) {
+			System.err.println("DB connection failed.");
+			return;
+		}
+		String sql = "INSERT IGNORE INTO history(user_id, item_id) VALUES (?, ?)";// use ? to prevent sql injection
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			for (String itemId : itemIds) {
+				ps.setString(2, itemId);
+				ps.execute();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void unsetFavoriteItems(String userId, List<String> itemIds) {
+		if (conn == null) {
+			System.err.println("DB connection failed.");
+			return;
+		}
+		String sql = "DELETE FROM history WHERE user_id = ? AND item_id = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			for (String itemId : itemIds) {
+				ps.setString(2, itemId);
+				ps.execute();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Set<String> getFavoriteItemIds(String userId) {
+		if (conn == null) {
+			return new HashSet<>();
+		}
+
+		Set<String> favoriteItems = new HashSet<>();
+
+		try {
+			String sql = "SELECT item_id FROM history WHERE user_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, userId);
+
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				String itemId = rs.getString("item_id");
+				favoriteItems.add(itemId);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return favoriteItems;
+
+	}
+
+	@Override
+	public Set getFavoriteItems(String userId) {
+		return null;
+
+	}
+
+	@Override
+	public Set<String> getCategories(String itemId) {
+		if (conn == null) {
+			return null;
+		}
+		Set<String> categories = new HashSet<>();
+		try {
+			String sql = "SELECT category from categories WHERE item_id = ? ";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, itemId);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				String category = rs.getString("category");
+				categories.add(category);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return categories;
+
+	}
+
+	@Override
+	public List searchItems(double lat, double lon, String term) {
+		return null;
+	}
+
+	@Override
+	public String getFullname(String userId) {
+		if (conn == null) {
+			return "";
+		}
+		String name = "";
+		try {
+			String sql = "SELECT first_name, last_name FROM users WHERE user_id = ?"; // use ? to prevent sql injection
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				name = rs.getString("first_name") + " " + rs.getString("last_name");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return name;
+	}
+
+	@Override
+	public boolean verifyLogin(String userId, String password) {
+		if (conn == null) {
+			return false;
+		}
+		try {
+			String sql = "SELECT user_id FROM users WHERE user_id = ? AND password = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			statement.setString(2, password);
+
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+
+}
